d141c426c535abd4cd989373de3548ab52b33b1d
94967 <949671041@qq.om>
2024-05-14 11:45:01 +0800

    部分修改20240514-中午

---
+++ 电能表软件研发管理系统最新/src/main/java/com/example/wisdom/mapper/VmRolePermissionMapper.java
@@ -0,0 +1,9 @@
+package com.example.wisdom.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.wisdom.entity.VmRolePermission;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface VmRolePermissionMapper extends BaseMapper<VmRolePermission> {
+}
